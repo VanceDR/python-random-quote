1af347dfa7b647ec8cfa98f9ed2c3f0d7c328682
VanceDelRosario <vancedelrosario@yahoo.com>
2022-06-18 20:38:11 +0800

    Added new features and new quotes to the list

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,5 @@
 import random
+import string
 def primary():
   # print("Keep it logically awesome.")
 
@@ -6,8 +7,22 @@ def primary():
   quotes = f.readlines()
   f.close()
   last = 13
+  # Removing Newline (\n) using .strip()
+  # First Quote
   rnd = random.randint(0,last)
-  print(quotes[rnd])
+  noNewLine = quotes[rnd].strip()
+  print(noNewLine)
+
+  # Second Quote
+  rnd = random.randint(0,last)
+  noNewLine = quotes[rnd].strip()
+  print(noNewLine)
+
+  newQuote = str(input("Type out New Quote Here: "))
+  f = open("quotes.txt", 'a') # 'a' for append
+  f.write("\n") # add new line to add in the next line
+  f.write(newQuote) # remove newline
+  f.close() # close file
 
 if __name__== "__main__":
   primary()
